edaed57c6aefa67b527fd2532dcd62d6b5109f76
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-02-26 07:06:11 -0300

    added draw function

--- index.js
+++ index.js
@@ -18,3 +18,16 @@ ctx.rect(160, 10, 100, 40);
 ctx.strokeStyle = "rgb(0 0 255 / 50%)";
 ctx.stroke();
 ctx.closePath();
+
+function draw() {
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+    
+ctx.beginPath();
+ctx.arc(50, 50, 10, 0, Math.PI * 2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath();
+}
+
+setInterval(draw, 10);
