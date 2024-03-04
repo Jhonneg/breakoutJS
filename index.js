efb7727659efe3c2faf02688b350ba0aa55295b7
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-04 09:09:25 -0300

    added colision

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 const canvas = document.querySelector("#myCanvas");
-const ctx = canvas.getContext("2d")
-
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(240, 140, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgb(0 0 255 / 50%)";
-ctx.stroke();
-ctx.closePath();
-
+const ctx = canvas.getContext("2d");
+const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
+let dx = 3;
+let dy = 2;
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function draw() {
-ctx.beginPath();
-ctx.arc(50, 50, 10, 0, Math.PI * 2);
-ctx.fillStyle = "#0095DD";
-ctx.fill();
-    ctx.closePath();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
-
-setInterval(draw, 10);
+function startGame() {
+  const interval = setInterval(draw, 10);
+}
+document.querySelector("button").addEventListener("click", function () {
+  startGame();
+  this.disabled = true;
+});
