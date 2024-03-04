8379eadda54f4545d9519066a25be97a1a2cef9e
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-04 09:48:48 -0300

    added bricks

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const ctx = canvas.getContext("2d");
 const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-let dx = 3;
+let dx = -3;
 let dy = 2;
 const paddleHeight = 10;
 const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
 
 
 function drawBall() {
@@ -26,9 +41,25 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+      for (let r = 0; r < brickRowCount; r++) {
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = 0;
+      bricks[c][r].y = 0;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -37,10 +68,13 @@ function draw() {
     else if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
+        } 
+        if (x > brickX && x < brickX + brickWidth) {
+          dy = -dy;
         } else {
-            alert('Game Over')
-            document.location.reload();
-            clearInterval(interval);
+          alert("Game Over");
+          document.location.reload();
+          clearInterval(interval);
         }
     }
     
